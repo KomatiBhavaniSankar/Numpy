2470c8ee8a77ed4a29e36035e150a7be5605c556
KomatiBhavaniSankar <bhavanisankarkomati@gmail.com>
2025-07-31 23:41:19 +0530

    day 1

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 import numpy as np
-
+'''
 a_mul = np.array([[1,2,3],
 [4,5,6],
 [7,8,9]
 
-
-
 ])
+
+print(a_mul)
+
+print(a_mul.ndim)
+print(a_mul.size)
+print(a_mul.dtype)
+
+
 
 print(a_mul[0])
 print(a_mul[0,1])
+print(a_mul[1])
+print(a_mul.shape)
+'''
+
+
+
+
+#a = np.array([[1,2,3],[4,"hello",5],[6,7,8]
+
+a = np.array([[1,2,3],[4,"9",5],[6,7,8]
+
+])
+
+print(a.dtype)
+print(type(a[0][0].dtype))
