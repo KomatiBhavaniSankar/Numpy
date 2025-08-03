57e0628ff201b323327f1a005cafc98741fa2df1
KomatiBhavaniSankar <bhavanisankarkomati@gmail.com>
2025-08-04 01:00:52 +0530

    progress

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ print(a_mul[0])
 print(a_mul[0,1])
 print(a_mul[1])
 print(a_mul.shape)
-'''
+
 
 d = {'1':'A'}
 
@@ -31,3 +31,38 @@ a = np.array([[1,2,3],[4,d,5],[6,7,"hello"]
 
 print(a.dtype)
 print(type(a[2][2]))
+
+a = np.full((2,10,4), 9 )
+print(a)
+
+a = np.zeros((10,5,4))
+print(a)
+
+a = np.ones((10,5,4))
+print(a)
+
+
+a = np.empty((10,5,4))
+print(a)
+
+
+x_values =  np.arange(0,1000,5)
+print(x_values)
+
+x_values = np.linspace(0,100,1001)
+print(x_values)
+
+
+print(np.nan)
+print(np.inf)
+print(np.isnan(np.nan))
+print(np.isinf(np.inf))
+'''
+l1 = [1,2,3,4,5]
+l2=[6,7,8,9,0]
+
+a1 = np.array(l1)
+a2 = np.array(l2)
+
+print(l1*5)
+print(a1*5)
