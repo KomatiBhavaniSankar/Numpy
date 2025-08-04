b330d70cad64d75821ff4d16be04f59a0b75d1fb
KomatiBhavaniSankar <bhavanisankarkomati@gmail.com>
2025-08-04 23:50:13 +0530

    progress

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ print(np.nan)
 print(np.inf)
 print(np.isnan(np.nan))
 print(np.isinf(np.inf))
-'''
+
 l1 = [1,2,3,4,5]
 l2=[6,7,8,9,0]
 
@@ -66,3 +66,42 @@ a2 = np.array(l2)
 
 print(l1*5)
 print(a1*5)
+
+print(l1+l2)
+
+print(a1+a2)
+print(a1*a2)
+
+a1 = np.array([1,2,3])
+a2 = np.array([[1],
+[2]])
+print(a1+a2)
+
+
+a  = np.array([[1,2,3],
+[4,5,6]])
+
+print(np.square(a))
+print(np.sort(a))
+
+
+
+
+print(a.transpose())
+
+print(a.shape)
+print(a.reshape((3,4)))
+print(a.flatten())
+print(a.ravel())
+'''
+a1  = np.array([[1,2,3],
+[4,5,6]])
+
+a2= np.array([[7,8,9],
+[11,12,13]])
+a = np.stack((a1,a2))
+
+print(a)
+#a = np.concatenate((a1,a2), axis=0)
+#print(a)
+
